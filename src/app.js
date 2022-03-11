ed8fd54f97e69211fb1731821916c5fc4ac4c266
Mustafa Baser <mkursatbaser@gmail.com>
2022-03-11 10:49:00 +0300

    Express.js Update

--- src/app.js
+++ src/app.js
@@ -6,16 +6,25 @@ console.log(__dirname) //directory name, to get the directory path
 const app = express()
 const publicDirectoryPath = path.join(__dirname, '../public')
 
-app.use(express.static(publicDirectoryPath))
-
-
-app.get('', (req, res) => { // req = request, res = response
-    res.send('<h1>This is a title</h1>')
-})
-
 app.listen(3000, () => {
     console.log('Server is up on port 3000.') //3000 default port
 }) 
+
+
+app.use(express.static(publicDirectoryPath))
+
+app.get('/weather', (req, res) => {
+    res.send({
+        location: 'Izmir, Turkey',
+        forecast: 'Partly cloud and 3 degrees.'
+    })
+})
+
+
+/*
+app.get('', (req, res) => { // req = request, res = response
+    res.send('<h1>This is a title</h1>')
+})
 
 app.get('/help', (req, res) => {
     res.send({
@@ -28,11 +37,4 @@ app.get('/help', (req, res) => {
 
 app.get('/about', (req, res) => {
     res.send('<h1>About myself</h1><p>Hello, my name is Mustafa. This is my about page.</p>')
-})
-
-app.get('/weather', (req, res) => {
-    res.send({
-        location: 'Izmir, Turkey',
-        forecast: 'Partly cloud and 3 degrees.'
-    })
-})
+})*/
