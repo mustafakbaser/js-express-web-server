9bfeb59eb58dabd04748d7348e70f57a9c9abc91
Mustafa Baser <mkursatbaser@gmail.com>
2022-03-11 10:24:06 +0300

    Express.js Update

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('', (req, res) => { // req = request, res = response
-    res.send('Hello express!')
+    res.send('<h1>This is a title</h1>')
 })
 
 app.listen(3000, () => {
@@ -11,13 +11,21 @@ app.listen(3000, () => {
 }) 
 
 app.get('/help', (req, res) => {
-    res.send('Help page')
+    res.send({
+        name: 'Mustafa',
+        age: 25,
+        title: 'Software Engineer'
+
+    })
 })
 
 app.get('/about', (req, res) => {
-    res.send('Hi, I am Mustafa.')
+    res.send('<h1>About myself</h1><p>Hello, my name is Mustafa. This is my about page.</p>')
 })
 
 app.get('/weather', (req, res) => {
-    res.send('Partly cloud.')
+    res.send({
+        location: 'Izmir, Turkey',
+        forecast: 'Partly cloud and 3 degrees.'
+    })
 })
