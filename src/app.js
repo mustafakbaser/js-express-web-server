9042b7893d9014da9349f60127a4c012892f4dd0
Mustafa Baser <mkursatbaser@gmail.com>
2022-03-11 10:14:48 +0300

    Express.js Update

--- src/app.js
+++ src/app.js
@@ -6,4 +6,18 @@ app.get('', (req, res) => { // req = request, res = response
     res.send('Hello express!')
 })
 
-app.listen(3000) //3000 default port
+app.listen(3000, () => {
+    console.log('Server is up on port 3000.') //3000 default port
+}) 
+
+app.get('/help', (req, res) => {
+    res.send('Help page')
+})
+
+app.get('/about', (req, res) => {
+    res.send('Hi, I am Mustafa.')
+})
+
+app.get('/weather', (req, res) => {
+    res.send('Partly cloud.')
+})
