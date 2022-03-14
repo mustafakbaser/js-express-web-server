925eb555aa6ed6266a91d2af656cc01433b19a9d
Mustafa Baser <mkursatbaser@gmail.com>
2022-03-14 12:33:41 +0300

    Search Logic

--- public/js/app.js
+++ public/js/app.js
@@ -6,22 +6,11 @@ fetch('https://puzzle.mead.io/puzzle').then((response) => {
     })
 })
 
-/*
-fetch('http://localhost:3000/weather?address=boston').then((response) => {
-    response.json().then((data) => {
-        if(data.error){
-            console.log(data.error)
-        }else{
-            console.log(data.location)
-            console.log(data.forecast)
-        }
-    })
-})*/
-
-
 //Search Form
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
+
+//Message paragraphs in index.hbs
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
 
