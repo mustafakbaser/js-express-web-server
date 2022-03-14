2ac3b7acf4b11e34f525c67a9a6aeb052ee7f4cd
Mustafa Baser <mkursatbaser@gmail.com>
2022-03-14 12:00:52 +0300

    Search Logic

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 const path = require('path')
 const express = require('express')
 const hbs = require('hbs')
-const { title } = require('process')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
 
 //nodemon src/app.js -e js,hbs
 
@@ -10,13 +11,13 @@ const app = express()
 
 //Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
-const viewsPath = path.join(__dirname,'../templates/views') //if we need to change views folder name, we need to define it here (for example templates) 
-const partialsPath = path.join(__dirname,'../templates/partials')
+const viewsPath = path.join(__dirname, '../templates/views') //if we need to change views folder name, we need to define it here (for example templates) 
+const partialsPath = path.join(__dirname, '../templates/partials')
 
 //Setup handlebard engine and view location
 app.set('view engine', 'hbs')
 app.set('views', viewsPath) //also need to connect with viewsPath for using app.set('views', viewsPath)
-hbs.registerPartials(partialsPath) 
+hbs.registerPartials(partialsPath)
 
 //Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
@@ -49,16 +50,44 @@ app.get('/help', (req, res) => {
 })
 
 app.get('/weather', (req, res) => {
-    res.send({
-        location: 'Izmir, Turkey',
-        forecast: 'Partly cloud and 3 degrees.'
+    if (!req.query.address) {
+        return res.send({
+            error: 'You must specify an address!'
+        })
+    }
+
+    geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
+        if (error) {
+            return res.send({ error })
+        }
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error) {
+                return res.send({ error })
+            }
+            res.send({
+                forecast: forecastData,
+                location: location,
+                address: req.query.address
+            })
+        })
     })
+    // res.send({
+    //     location: 'Izmir, Turkey',
+    //     forecast: 'Partly cloud and 3 degrees.',
+    //     address: req.query.address
+    // })
 })
 
+//weather?address=izmir
+
 app.get('/products', (req, res) => {
-    res.send({
-        products: []
-    })
+    if (!req.query.search) {
+        return res.send({
+            error: 'You must provide a search term!'
+        })
+    }
+
+    console.log(req.query.search)
 })
 
 // 404 Page
@@ -75,7 +104,7 @@ app.get('/help/*', (req, res) => {
     res.send('Help article cannot be found!')
 })
 
-app.get('/help/*', (req, res)=>{
+app.get('/help/*', (req, res) => {
     res.render('404', {
         title: '404',
         name: 'Mustafa Baser',
